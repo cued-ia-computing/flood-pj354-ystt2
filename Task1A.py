6106b2934eeda0a4d1280bd93fb9219339deb8eb
Tungsten <ystt2@cam.ac.uk>
2021-01-30 23:39:40 +0000

    test 2

--- Task1A.py
+++ Task1A.py
@@ -17,7 +17,7 @@ def run():
     # Display data from 3 stations:
     for station in stations:
         if station.name in [
-                'Bourton Dickler', 'Surfleet Sluice', 'Oakington'
+                'Bourton Dickler', 'Girton', 'Oakington'
         ]:
             print(station)
 
