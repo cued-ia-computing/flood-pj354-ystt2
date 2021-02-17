bc2d89cb76a7bb787bf49a1f7a85ade51d50d700
pj354 <78282311+pj354@users.noreply.github.com>
2021-02-17 23:28:40 +0000

    i hope this works t-money :P

--- Task2F.py
+++ Task2F.py
@@ -1,2 +1,16 @@
 from floodsystem import analysis
+from floodsystem import station
+from floodsystem import stationdata
+from floodsystem import datafetcher
 
+stations = stationdata.build_station_list()
+station_1 = stations[1]
+print(station_1)
+id = station_1.measure_id
+dates, levels = datafetcher.fetch_measure_levels(id,48)
+#converting datetime into float
+dates_float = matplotlib.dates.date2num(dates)
+#plotting the thing now
+
+
+poly(dates_float, levels, 3)
--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -1,35 +1,23 @@
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-from datafetcher import fetch_measure_levels
-from station import MonitoringStation
-from stationdata import build_station_list
 
-
-stations = build_station_list()
-station_1 = stations[3]
-id = station_1.measure_id
-dates, levels = fetch_measure_levels(id,200)
-#converting datetime into float
-dates_float = matplotlib.dates.date2num(dates)
-#plotting the thing now
 
 def poly(dates_float, levels, p):
     #Finding the coefficients
     p_coeff = np.polyfit(dates_float, levels, p) 
     #Shifting the X axis 
-    _shifted = min(dates_float)
-    new_shifted_float = np.linspace(_shifted, _shifted, num=len(dates_float))
-    shifted = []
+    d0 = min(dates_float)
+    new_shifted_float = np.linspace(d0, d0, num=len(dates_float))
+    shifted_dates = []
     for i in range(0,len(dates_float)):
         new = dates_float[i] - new_shifted_float[i]
-        shifted.append(new)
+        shifted_dates.append(new)
     #Converting into polynomial
     poly = np.poly1d(p_coeff)
     #Now plot
-    plt.plot(shifted, poly(dates_float))
+    plt.plot(shifted_dates, poly(dates_float))
     plt.show()
 
 
 
-poly(dates_float, levels, 3)
